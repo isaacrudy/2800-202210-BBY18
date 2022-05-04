ccdd7f4ea0fbdd34ed747fcfdd962b5d2e25c13a
Isaac Rudy <isaac.rudy@gmail.com>
2022-05-04 09:02:19 -0700

    Edited login page and added images folder

--- sql/login.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE user_login (
-  ID int NOT NULL AUTO_INCREMENT,
-  username VARCHAR(50),
-  first_name VARCHAR(50),
-  last_name VARCHAR(50),
-  email VARCHAR(50),
-  password VARCHAR(50),
-  PRIMARY KEY (ID)
-);
