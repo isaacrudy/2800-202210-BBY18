ba8b11309c74f8405d9de91d57b8131dde6fc7f2
Calvin Yu <yucalvin5@gmail.com>
2022-05-05 13:02:59 -0700

    login function working for login.html

---
+++ JavaScript/database.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const session = require('express-session');
+const path = require('path');
+const mysql = require('mysql');
+
+const app = express();
+app.use(session({
+	secret: 'secret',
+	resave: true,
+	saveUninitialized: true
+}));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'static')));
+
+const connection = mysql.createConnection({
+	host     : 'localhost',
+	user     : 'root',
+	password : '',
+	database : 'mydb'
+});
+
+app.get('/', function(req, res) {
+	// Render login template
+	res.sendFile(path.join(__dirname + '/../html/login.html'));
+});
+
+app.post('/auth', function(req, res) {
+	// Capture the input fields
+	let email = req.body.username;
+	let password = req.body.password;
+	// Ensure the input fields exists and are not empty
+
+	if (email && password) {
+		// Execute SQL query that'll select the account from the database based on the specified username and password
+		connection.query('SELECT * FROM users WHERE email = ? AND password = ?', [email, password], function(error, results, fields) {
+			// If there is an issue with the query, output the error
+			if (error) throw error;
+			// If the account exists
+			if (results.length > 0) {
+				// Authenticate the user
+				req.session.loggedin = true;
+				req.session.username = email;
+				// Redirect to home page
+				res.redirect('/main');
+			} else {
+				res.send('Incorrect Email and/or Password!');
+			}			
+			res.end();
+		});
+	}
+     else {
+		response.send('Please enter Email and Password!');
+		res.end();
+	}
+});
+
+app.get('/main', function(request, response) {
+	// If the user is loggedin
+	if (request.session.loggedin) {
+		// Output username
+		response.send('Welcome back, ' + request.session.firstName + '!');
+	} else {
+		// Not logged in
+		response.send('Please login to view this page!');
+	}
+	response.end();
+});
+app.listen(5500, function(){
+    console.log('Listening on port 5500')
+   })
--- sql/login.sql
+++ sql/login.sql
@@ -1,9 +1,29 @@
-CREATE TABLE IF NOT EXISTS user(
-    id int AUTO_INCREMENT,
-    password     VARCHAR(50) NOT NULL,
-    firstName    VARCHAR(50) NOT NULL,
-    lastName     VARCHAR(50) NOT NULL,
-    email        VARCHAR(50) UNIQUE NOT NULL,
-    role         VARCHAR(20) NOT NULL DEFAULT "regular",
+CREATE DATABASE IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `mydb`;
+
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE IF NOT EXISTS users(
+    id                 int AUTO_INCREMENT,
+    password        VARCHAR(20) NOT NULL,
+    firstName       VARCHAR(50) NOT NULL,
+    lastName        VARCHAR(50) NOT NULL,
+    email           VARCHAR(50) UNIQUE NOT NULL,
+    profilePhoto    VARCHAR(50)    NOT NULL DEFAULT "some/filePath/default.png",
+    role            VARCHAR(7) NOT NULL DEFAULT "regular",
     PRIMARY KEY(id)
 );
+
+INSERT INTO users
+(password, firstName, lastName, email)
+VALUES
+    ("1234", "Dennis", "Relos", "drelos@gmail.com"),
+    ("asdf", "Calvin", "Yu", "cyu@gmail.com"),
+    ("12qw", "Isaac", "Rudy", "irudy@gmail.com"),
+    ("zx12", "Amadeus", "Min", "amin@gmail.com");
+    
+INSERT INTO users
+    (password, firstName, lastName, email, role)
+VALUES
+    ("admin", "Omar", "Navarro", "onavarro@fundcart.com", "admin"),
+    ("admin1234", "Buzz", "Lightyear", "blightyear@fundcart.com", "admin");
