f59b8a3dbe827328cc25f958bfa2144023a7d9ba
drelos00 <drelos00@gmail.com>
2022-05-13 01:23:12 -0700

    Fixed a bug that disabled clicking on edit and delete buttons in the table.

--- public/js/disable_scrolling.js
+++ public/js/disable_scrolling.js
@@ -1,7 +1,9 @@
 document.querySelector('.toggler').addEventListener('change', function() {
     if (this.checked) {
         document.body.style.overflowY = 'hidden'
+        document.getElementById("user_list_container").style.zIndex = '-1'
     }else {
         document.body.style.overflowY = '';
+        document.getElementById("user_list_container").style.zIndex = '0'
     }
 });
