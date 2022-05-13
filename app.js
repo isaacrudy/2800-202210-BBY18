c042c28a8c7601b3523a08e77e8102b3c0be1ee4
drelos00 <drelos00@gmail.com>
2022-05-13 11:50:40 -0700

    Added device scaling to all pages. Styled all forms for updating user info and creating new accounts.

--- app.js
+++ app.js
@@ -393,11 +393,11 @@ app.get("/currentAccountInfo", async function (req, res) {
 
 	const [user_rows, user_info] = await connection.query("SELECT * FROM users WHERE ID = " + req.session.user_id);
 
-	let updateInputHTML = '<label for="firstName">First Name: </label><input type="text" class="form_input" id="firstName" value="' + user_rows[0].firstName
-		+ '" required /><label for="lastName">Last Name: </label><input type="text" class="form_input" id="lastName" value="' + user_rows[0].lastName
-		+ '" required /><label for="email">Email: </label><input type="text" id="email" class="form_input" value="'
-		+ user_rows[0].email + '" required /><label for="password">Password: </label><input type="password" name="password" id="password" class="form_input" value="'
-		+ user_rows[0].password + '" required /><label for="password_confirm">Confirm password: </label><input type="password" name="password_confirm" id="password_confirm" class="form_input" value="'
+	let updateInputHTML = '<div><input type="text" class="form_input" id="firstName" value="' + user_rows[0].firstName
+		+ '" required /><input type="text" class="form_input" id="lastName" value="' + user_rows[0].lastName
+		+ '" required /></div><input type="text" id="email" class="form_input" value="'
+		+ user_rows[0].email + '" required /><input type="password" name="password" id="password" class="form_input" value="'
+		+ user_rows[0].password + '" required /><input type="password" name="password_confirm" id="password_confirm" class="form_input" value="'
 		+ user_rows[0].password + '" required /><h3 id="invalidPassword" class="invalidPassword"></h3><input id="updateBtn" type="button" class="form_input_submit" value="Update Account" />'
 
 	connection.end;
