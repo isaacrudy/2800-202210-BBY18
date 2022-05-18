99e6b078988877dc6b09fbd6b7e89f61455fb813
drelos00 <drelos00@gmail.com>
2022-05-18 13:04:04 -0700

    Created JavaScript for demo page easter egg.

---
+++ public/js/spilled_beans.js
@@ -0,0 +1,23 @@
+// const bean = document.getElementsByClassName('beans');
+
+// bean.addEventListener('mouseenter', function(){
+//     text.innerHTML =   `hello`
+// });
+
+const text = document.getElementById('bean-header');
+const beans = document.getElementById('beans');
+
+
+function spill()
+{
+    text.innerHTML = 'You spilled the beans!'
+    beans.style.background = 'url(img/demo_page/spilled_beans_small.png)'
+}
+
+function revert()
+{
+    setTimeout(() => {
+        text.innerHTML = 'Canned Beans'
+        beans.style.background = 'url(img/demo_page/tin_can_small.png)'
+    }, 2000);
+}
