77c2b3e50e3c9098c7fc2a49b761d23cb3ce7bec
drelos00 <drelos00@gmail.com>
2022-05-20 08:10:02 -0700

    Added hover effects to the "preset" donation amount options. Color also changes to highlight which option is active.

--- public/js/calculate_donation.js
+++ public/js/calculate_donation.js
@@ -5,6 +5,11 @@ let newTotal = 0;
 const donateTotal = document.getElementById('donation-total');
 const donateVal = document.getElementById('donation-amount');
 
+const nearDollar = document.getElementById('nDollar');
+const fP = document.getElementById('fiveP');
+const tP = document.getElementById('tenP');
+const twP = document.getElementById('twentyP');
+
 function getNewTotal() {
     donateVal.value = donateAmount;
     newTotal = (+donateAmount) + (+stripped);
@@ -16,21 +21,37 @@ function nearestDollar() {
     let cents = stripped.substring(2);
     donateAmount = 1 - cents;
     getNewTotal();
+    nearDollar.style.background = "grey";
+    fP.style.background = "var(--fc_teal)";
+    tP.style.background = "var(--fc_teal)";
+    twP.style.background = "var(--fc_teal)";
 }
 
 function fivePercent() {
     donateAmount = (stripped * 0.05).toFixed(2);
     getNewTotal();
+    fP.style.background = "grey";
+    nearDollar.style.background = "var(--fc_teal)";
+    tP.style.background = "var(--fc_teal)";
+    twP.style.background = "var(--fc_teal)";
 }
 
 function tenPercent() {
     donateAmount = (stripped * 0.1).toFixed(2);
     getNewTotal();
+    tP.style.background = "grey";
+    nearDollar.style.background = "var(--fc_teal)";
+    fP.style.background = "var(--fc_teal)";
+    twP.style.background = "var(--fc_teal)";
 }
 
 function twentyPercent() {
     donateAmount = (stripped * 0.2).toFixed(2);
     getNewTotal();
+    twP.style.background = "grey";
+    nearDollar.style.background = "var(--fc_teal)";
+    fP.style.background = "var(--fc_teal)";
+    tP.style.background = "var(--fc_teal)";
 }
 
 function customAmount() {
